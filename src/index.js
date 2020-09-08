ef542d1469b35dba6391c234a3f0bf486df53eb2
iamfotx <iamfotx@gmail.com>
2020-09-08 22:15:46 +0500

    setup-mac

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 'use strict'
 const os = require('os')
+const shell = require('shelljs')
 const whoami = os.userInfo().username
 
+shell.exec('clear')
 console.log(
   `Hey ${whoami}, go get some food 🍕🌮 while I'm setting up your Mac!`,
 )
